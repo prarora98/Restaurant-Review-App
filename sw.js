d500aba20c3d838cd7a4835104859460c665fd25
arshisaleh <arshisaleh@gmail.com>
2018-09-26 11:48:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -50,13 +50,9 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response){
-            if(response){
-                return response;
-            }
-           return fetch(event.request);
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
         })
-    )
+    );
 });
-
-
+        
