8ad63c6d0694fef00aa111788feb0e5b86672e9f
arshisaleh <arshisaleh@gmail.com>
2018-09-26 11:49:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -50,9 +50,16 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+        caches.match(event.request).then(function(response){
+            if(response){
+                return response;
+            }
+           return fetch(event.request);
         })
-    );
+    )
 });
+
+
+
+       
         
